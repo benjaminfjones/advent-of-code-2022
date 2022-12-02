1cec7636737ef082f67f40685698f44443429c85
Benjamin F Jones <benjaminfjones@gmail.com>
2022-12-01 22:30:12 -0800

    Move some common code to Aoc2022_lib

--- bin/day1.ml
+++ bin/day1.ml
@@ -1,4 +1,5 @@
 open Core
+open Aoc2022_lib
 
 let parse_input lines =
   let proc acc x =
@@ -12,19 +13,6 @@ let parse_input lines =
   in
   List.fold_left lines ~init:[ [] ] ~f:proc
 
-let int_sum = List.fold_left ~init:0 ~f:( + )
-
-let int_max xs =
-  match xs with
-  | [] -> None
-  | x0 :: rest0 ->
-      let rec aux max_so_far xs =
-        match xs with
-        | [] -> max_so_far
-        | x :: rest ->
-            if x > max_so_far then aux x rest else aux max_so_far rest
-      in
-      Some (aux x0 rest0)
 let largest_sum lss =
   let sums = List.map ~f:int_sum lss in
   match int_max sums with
--- lib/aoc2022_lib.ml
+++ lib/aoc2022_lib.ml
@@ -1,3 +1,19 @@
+open Core
+
 module type Solver = sig
   val solve : string Array.t -> string list -> int
 end
+
+let int_sum = List.fold_left ~init:0 ~f:( + )
+
+let int_max xs =
+  match xs with
+  | [] -> None
+  | x0 :: rest0 ->
+      let rec aux max_so_far xs =
+        match xs with
+        | [] -> max_so_far
+        | x :: rest ->
+            if x > max_so_far then aux x rest else aux max_so_far rest
+      in
+      Some (aux x0 rest0)
