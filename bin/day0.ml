8ddb58548e5f4ddd6fde7615090b2ef4c4e7a7ae
Benjamin F Jones <benjaminfjones@gmail.com>
2022-12-19 14:14:42 -0800

    Enable ppx_jane preprocessors

--- bin/day0.ml
+++ bin/day0.ml
@@ -6,12 +6,12 @@ let rec last = function
     | x :: [] -> Some x
     | _ :: rest -> last rest
 
-let%test "last a b c d" =
-    let r = last ["a"; "b"; "c"; "d"] in
-    Option.equal String.equal r (Some "d")
-let%test "last []" =
-    let r = last [] in
-    Option.equal String.equal r None
+(* let%test "last a b c d" = *)
+(*     let r = last ["a"; "b"; "c"; "d"] in *)
+(*     Option.equal String.equal r (Some "d") *)
+(* let%test "last []" = *)
+(*     let r = last [] in *)
+(*     Option.equal String.equal r None *)
 
 
 (* last two elements of a list *)
@@ -21,12 +21,12 @@ let rec last_two = function
     | x :: y :: [] -> Some (x, y)
     | _ :: rest -> last_two rest
 
-let%test_unit "last_two 4-list" =
-    [%test_eq: (string * string) option]
-      (last_two ["a"; "b"; "c"; "d"])
-      (Some ("c", "d"))
-let%test_unit "last_two singleton" =
-    [%test_eq: (string * string) option] (last_two ["a"]) None
+(* let%test_unit "last_two 4-list" = *)
+(*     [%test_eq: (string * string) option] *)
+(*       (last_two ["a"; "b"; "c"; "d"]) *)
+(*       (Some ("c", "d")) *)
+(* let%test_unit "last_two singleton" = *)
+(*     [%test_eq: (string * string) option] (last_two ["a"]) None *)
 
 (* Find the N'th element of a list. *)
 let rec nth xs n =
@@ -35,10 +35,10 @@ let rec nth xs n =
     | (_, n) when n < 0 -> raise (Failure "negative number")
     | (x :: _, 0) -> x
     | (_ :: rest, n) -> nth rest (n - 1)
-let%test_unit "nth second of 5" =
-    [%test_eq: string] (nth ["a"; "b"; "c"; "d"; "e"] 2) "c"
-let%test "nth second of 1" =
-    let ignore_str _ = false in
-    try ignore_str (nth ["a"] 2) with
-    | Failure _ -> true
-    | _ -> false
+(* let%test_unit "nth second of 5" = *)
+(*     [%test_eq: string] (nth ["a"; "b"; "c"; "d"; "e"] 2) "c" *)
+(* let%test "nth second of 1" = *)
+(*     let ignore_str _ = false in *)
+(*     try ignore_str (nth ["a"] 2) with *)
+(*     | Failure _ -> true *)
+(*     | _ -> false *)
